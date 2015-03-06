7bc316ec6fa8d0de21f439e5c559c632f1f13aed
Patrik Votocek <patrik@votocek.cz>
2015-03-06 14:17:21 +0100

    Add travis (CS, Lint)

---
+++ .travis.composer.php
@@ -0,0 +1,19 @@
+<?php
+
+$version = getenv('NETTE');
+
+if (!$version || $version == 'default') {
+	exit;
+}
+
+echo "Nette version " . $version . PHP_EOL;
+
+$file = __DIR__ . '/composer.json';
+$content = file_get_contents($file);
+$composer  = json_decode($content, TRUE);
+if (!isset($composer['require']['tracy/tracy'])) {
+	exit(255);
+}
+$composer['require']['tracy/tracy'] = $version;
+$content = json_encode($composer);
+file_put_contents($file, $content);
